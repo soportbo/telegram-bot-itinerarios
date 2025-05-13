5da627e439b0d2258e06419b97ac037b399cb61e
soportbo <soportetbo@gmail.com>
2025-05-13 14:41:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("7479654011:AAGz1neoiROjk603ig_lIv1iBDN5pTAr3Xc")  # Render cargará esto como variable de entorno
+TOKEN = os.getenv("7479654011:AAFoE2iCIPofdEu53qqQyvsAj-Eg8l7EXRI")  # Render cargará esto como variable de entorno
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("¡Hola! Soy tu bot de itinerarios y estoy funcionando 24/7 en Render.")
